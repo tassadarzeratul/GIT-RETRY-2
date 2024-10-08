b3c7fb0437a1b71baf6effff9ba0aeee6d519083
JihunJeon <iamjjh30@gmail.com>
2024-10-08 10:04:57 +0900

    df

--- JS/mainpage/detail2.js
+++ JS/mainpage/detail2.js
@@ -1,5 +1,5 @@
 // Join 화면으로 이동
-const moveToJoin = () => {
-    location.replace('../../HTML/login-join/join.html');
-}
-joinBtn.addEventListener('click', moveToJoin);
+// const moveToJoin = () => {
+//     location.replace('../../HTML/login-join/join.html');
+// }
+// joinBtn.addEventListener('click', moveToJoin);
