86ffa018d3aed9701c1934abf4a8a12cfa497bab
JihunJeon <iamjjh30@gmail.com>
2024-10-08 15:03:52 +0900

    sdf

--- JS/mainpage/main.js
+++ JS/mainpage/main.js
@@ -1,8 +1,20 @@
 // Join 화면으로 이동
-const moveToJoin = () => {
+const mainToJoin = () => {
     location.replace('../../HTML/login-join/join.html');
 }
-joinBtn.addEventListener('click', moveToJoin);
+joinBtn.addEventListener('click', mainToJoin);
+
+// Login 화면으로 이동
+const mainToLogin = () => {
+    location.replace('../../HTML/login-join/login.html');
+}
+loginBtn.addEventListener('click', mainToLogin);
+
+// Login 화면으로 이동
+const mainToMypage = () => {
+    location.replace('../../HTML/mypage/김건우_mypage.html');
+}
+myPageBtn.addEventListener('click', mainToMypage);
 
 
 // main slider 동작구현 =================================================================
@@ -31,70 +43,32 @@ setInterval(slideImages, 3000);
 
 // main slider 동작구현 끝 =================================================================
 
-// document.querySelectorAll('sub .pic_container').forEach(container => {
-//     const picList = container.querySelector('.pic_list');
-//     const images = picList.querySelectorAll('li img');
-//     let currentIndex = 0;
-
-//     container.querySelector('.btn1').addEventListener('click', () => {
-//         console.log('.list');
-//         currentIndex = (currentIndex - 1 + images.length) % images.length; // 이전 이미지로 이동        
-//         container.querySelector(".list").style.display = "none";
-//         container.querySelector("#list" + currentIndex).style.display = "block"; // display : block;
-//     });
-
-//     container.querySelector('.btn2').addEventListener('click', () => {
-//         currentIndex = (currentIndex + 1) % images.length; // 다음 이미지로 이동 (0)
-//         container.querySelector(".list").style.display = "none";
-//         container.querySelector("#list" + currentIndex).style.display = "block"; // display : block;
-//     });
-// });
-
-
-// document.querySelectorAll('sub .pic_container').forEach(container => {
-//     const picList = container.querySelector('.pic_list');
-//     const images = picList.querySelectorAll('li img');
-//     const allLists = container.querySelectorAll('.list'); // 모든 list 요소들 가져오기
-//     let currentIndex = 0;
-
-//     const updateDisplay = () => {
-//         allLists.forEach(list => list.style.display = "none"); // 모든 리스트 숨기기
-//         allLists[currentIndex].style.display = "block"; // 현재 인덱스 리스트 표시
-//     };
-
-//     // updateDisplay(); // 첫 번째 이미지 표시
-
-//     container.querySelector('.btn1').addEventListener('click', () => {
-//         currentIndex = (currentIndex - 1 + images.length) % images.length; // 이전 이미지로 이동        
-//         updateDisplay(); // 화면 업데이트
-//     });
-
-//     container.querySelector('.btn2').addEventListener('click', () => {
-//         currentIndex = (currentIndex + 1) % images.length; // 다음 이미지로 이동
-//         updateDisplay(); // 화면 업데이트
-//     });
-// });
+// sub slider 동작구현 =================================================================
 
 document.querySelectorAll('sub .pic_container').forEach(container => {
+    // 각 슬라이드 영역의 클래스명 pic_container
     const picList = container.querySelector('.pic_list');
     const images = picList.querySelectorAll('li img');
-    const liIndex = container.querySelectorAll(".list"); // class명 list인 li태그         
+    const liIndex = container.querySelectorAll("li"); // class명 list인 li태그         
     let currentIndex = 0;
 
-    container.querySelector('.btn1').addEventListener('click', () => {
-        currentIndex = (currentIndex - 1 + images.length) % images.length; // 이전 이미지로 이동        
-        // picList.style.transform = 'translate(0vw)'; // 슬라이드 이동
+    container.querySelector('.prevBtn').addEventListener('click', () => {
+        currentIndex = (currentIndex - 1 + images.length) % images.length;
+        // 이전 이미지로 이동        
         for (let i = 0; i < liIndex.length; i++) { // li태그 전체 숨김 처리
             liIndex[i].style.display = "none";
         }
-        container.querySelector("#list" + currentIndex).style.display = "block"; // display : block; 
+        liIndex[currentIndex].style.display = "block";
     });
 
-    container.querySelector('.btn2').addEventListener('click', () => {
-        currentIndex = (currentIndex + 1) % images.length; // 다음 이미지로 이동 (0)        
+    container.querySelector('.nextBtn').addEventListener('click', () => {
+        currentIndex = (currentIndex + 1) % images.length;
+        // 다음 이미지로 이동 (0)        
         for (let i = 0; i < liIndex.length; i++) { // li태그 전체 숨김 처리
             liIndex[i].style.display = "none";
         }
-        container.querySelector("#list" + currentIndex).style.display = "block"; // 하나의 li만 보이기
+        liIndex[currentIndex].style.display = "block";
     });
 });
+
+// sub slider 동작구현 끝 =================================================================
