0bae009aec7af624457101252be1f1d2bf8fac7b
junggyu <jungkyu201@naver.com>
2024-10-08 20:15:18 +0900

    dsf

--- JS/admin/admin.js
+++ JS/admin/admin.js
@@ -1,8 +1,5 @@
 // main 화면으로 이동
-const adminToMainBtn = document.getElementById('adminToMainBtn');
-
 const adminToMain = () => {
-    location.replace('../../HTML/mainpage/main.html');
+    window.open('../../HTML/mainpage/main.html');
 }
 adminToMainBtn.addEventListener('click', adminToMain);
-
