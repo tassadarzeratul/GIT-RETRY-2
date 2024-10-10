85e4fb15f48ff62c17659e3c84d953bf6e6555bc
tassadarzeratul <apmidbug@naver.com>
2024-10-10 17:29:07 +0900

    gnw commit

---
+++ JS/register/김건우_upload.js
@@ -0,0 +1,20 @@
+function setThumbnail(event) {
+    for (var image of event.target.files) {
+        var reader = new FileReader();
+
+        reader.onload = function (event) {
+            var img = document.createElement("img");
+            img.setAttribute("src", event.target.result);
+            document.querySelector("div#image_container").appendChild(img);
+        };
+        console.log(image);
+        reader.readAsDataURL(image);
+    }
+}
+
+const textarea = useRef();
+
+const handleResizeHeight = () => {
+  textarea.current.style.height = 'auto'; //height 초기화
+  textarea.current.style.height = textarea.current.scrollHeight + 'px';
+};
