843fb89ba99e843b27a708918b92a51fe27e6d4c
junggyu <jungkyu201@naver.com>
2024-10-16 22:13:48 +0900

    dfadsa

--- JS/mainpage/common.js
+++ JS/mainpage/common.js
@@ -1,24 +1,4 @@
-// 템플릿
-function includeHTML() {
-    const includes = document.querySelectorAll('[data-include]');
-
-    includes.forEach(async (el) => {
-        const file = el.getAttribute('data-include');
-        const response = await fetch(file);
-
-        if (response.ok) {
-            const text = await response.text();
-            el.innerHTML = text;
-
-            // 템플릿이 로드된 후 로그인 상태에 따른 버튼 표시/숨김 처리
-            executeAfterTemplateLoad();
-        } else {
-            el.innerHTML = 'Content not found!';
-        }
-    });
-}
-
-function executeAfterTemplateLoad() {
+document.addEventListener('DOMContentLoaded', () => {
     const joinBtn = document.getElementById('joinBtn');
     const loginBtn = document.getElementById('loginBtn');
     const mypageBtn = document.getElementById('mypageBtn');
@@ -33,25 +13,17 @@ function executeAfterTemplateLoad() {
         if (loginBtn) loginBtn.style.display = 'none';
         if (logoutBtn) logoutBtn.style.display = 'block';
         if (mypageBtn) mypageBtn.style.display = 'block';
+
     } else {
         if (joinBtn) joinBtn.style.display = 'block';
         if (loginBtn) loginBtn.style.display = 'block';
         if (logoutBtn) logoutBtn.style.display = 'none';
         if (mypageBtn) mypageBtn.style.display = 'none';
     }
+});
 
-    // 로그아웃 버튼 클릭 시 로그아웃 처리
-    logoutBtn?.addEventListener('click', () => {
-        localStorage.setItem('isLoggedIn', 'false');
-        location.reload();
-    });
-}
-
-document.addEventListener('DOMContentLoaded', includeHTML);
-
-
-
-// ==================================================================================
-
-
-
+// 로그아웃 버튼 클릭 시 로그아웃 처리
+document.getElementById('logoutBtn')?.addEventListener('click', () => {
+    localStorage.setItem('isLoggedIn', 'false');
+    location.reload();
+});
