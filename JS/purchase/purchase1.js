920dc0778a9e7c95d4982c3946b0e3bc1d98dfc4
eblish159 <tkdgud159@naver.com>
2024-10-16 15:18:43 +0900

    fire

--- JS/purchase/purchase1.js
+++ JS/purchase/purchase1.js
@@ -1,3 +1,43 @@
+// main 화면으로 이동
+const mainToGo = (url) => {
+  window.open(url);
+}
+// 로고
+$('#logo_picMain').on('click', () => mainToGo('../../HTML/mainpage/main.html'));
+// 홈
+$('#mainToHome').on('click', () => mainToGo('../../HTML/mainpage/main.html'));
+// 펀딩등록
+$('#mainToRegister').on('click', () => mainToGo('../../HTML/register/register.html'));
+// 펀딩리스트
+$('#mainToList').on('click', () => mainToGo('../../HTML/mainpage/pagelist.html'));
+// 종료펀딩
+$('#mainToend').on('click', () => mainToGo('../../HTML/register/end.html'));
+// 작품구매
+$('#mainTopurchase_1').on('click', () => mainToGo('../../HTML/purchase/purchase1.html'));
+// join
+$('#joinBtn').on('click', () => mainToGo('../../HTML/login-join/join.html'));
+// Login
+$('#loginBtn').on('click', () => mainToGo('../../HTML/login-join/login.html'));
+// myPage
+$('#mypageBtn').on('click', () => mainToGo('../../HTML/mypage/mypage.html'));
+// 서브 사진
+$('.pic_list').on('click', () => mainToGo('../../HTML/mainpage/detail.html'));
+// 전체보기
+$('#allPic').on('click', () => mainToGo('../../HTML/mainpage/pagelist.html'));
+// 오른쪽 사진
+$('.right .pic').on('click', () => mainToGo('../../HTML/mainpage/detail.html'));
+// 아래쪽 사진
+$('.otherRight img').on('click', () => mainToGo('../../HTML/mainpage/detail.html'));
+
+
+
+
+
+
+
+
+
+//슬라이드
 var slideIndex = 1;
 showSlides(slideIndex);
 
