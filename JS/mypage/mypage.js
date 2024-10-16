87cec7e9ba74d66ccc1dfb1b2c0b082c58e096ca
myname <tkdgud159@naver.com>
2024-10-16 22:57:48 +0900

    fire

--- JS/mypage/mypage.js
+++ JS/mypage/mypage.js
@@ -13,10 +13,11 @@ $('#myPageToend').on('click', () => myPageToGo('../../HTML/register/end.html'));
 // 작품구매
 $('#myPageTopurchase_1').on('click', () => myPageToGo('../../HTML/purchase/purchase1.html'));
 // 작품등록
-$('endTopurchaseregister_1').on('click', () => endToGo('../../HTML/register/productregister.html'));
+$('endTopurchaseregister_1').on('click', () => myPageToGo('../../HTML/register/productregister.html'));
 // join
 $('#joinBtn').on('click', () => myPageToGo('../../HTML/login-join/join.html'));
 // Login
 $('#loginBtn').on('click', () => myPageToGo('../../HTML/login-join/login.html'));
 // myPage
 $('#mypageBtn').on('click', () => myPageToGo('../../HTML/mypage/mypage.html'));
+
